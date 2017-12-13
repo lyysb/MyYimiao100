60866247699beadb4e11fad2cf21453beb7c65b6
MichelPro <1639692449@qq.com>
2017-12-13 11:25:16 +0800

    test

---
+++ app/src/main/java/com/yimiao100/sale/annotation/Poko.kt
@@ -0,0 +1,5 @@
+package com.yimiao100.sale.annotation
+
+/**
+ * Created by Michel on 2017/4/14.
+ */
---
+++ app/src/main/java/com/yimiao100/sale/bean/Poko.kt
@@ -0,0 +1,5 @@
+package com.yimiao100.sale.bean
+
+/**
+ * Created by Michel on 2017/4/14.
+ */
